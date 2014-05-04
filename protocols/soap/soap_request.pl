b5f1377a729bb934697d3b07e3b26afa4d58f6f1
Erik Kunze <eku@users.sourceforge.net>
2014-05-04 18:48:04 +0200

    check for failure of request

--- protocols/soap/soap_request.pl
+++ protocols/soap/soap_request.pl
@@ -8,9 +8,15 @@ my $soap = SOAP::Lite
     -> proxy('http://192.168.22.130/soap');
 
 my $whm = $soap->whm();
-
-print "Uptime of the device: ", $whm, "\n";
+unless ($whm->fault) {
+  print "Uptime of the device: ", $whm->result, "\n";
+} else {
+  print join ', ', $whm->faultcode, $whm->faultstring, $whm->faultdetail;
+}
  
 my $time = $soap->time();
-
-print "System time of the device in seconds since 1.1.1970: ", $time, "\n";
+unless ($time->fault) {
+  print "System time of the device in seconds since 1.1.1970: ", $time->result, "\n";
+} else {
+  print join ', ', $whm->faultcode, $whm->faultstring, $whm->faultdetail;
+}
